d0e2115fb393d63544f2c29c822bdf771a557271
Gergo Erdosi <gergo@timble.net>
2013-01-09 01:12:33 +0100

    Add default attribute and recipe files. re #73

---
+++ code/cookbooks/apache2-custom/attributes/default.rb
@@ -0,0 +1,1 @@
+default['apache']['sites_dir'] = '/var/www'
---
+++ code/cookbooks/apache2-custom/recipes/default.rb
@@ -0,0 +1,27 @@
+node['apache']['sites'].each do |site|
+  bash "create_symlink_#{site['name'].gsub(/-\./, '_')}" do
+    user 'root'
+    group 'root'
+    cwd "#{node['apache']['sites_dir']}/#{site['name']}"
+    code <<-EOH
+      [ -L public ] || ([ -d source/code ] && (ln -s source/code public) || (ln -s source public))
+    EOH
+  end
+  
+  template "#{node['apache']['dir']}/sites-available/#{site['name']}" do
+    source 'site.erb'
+    owner 'root'
+    group node['apache']['root_group']
+    mode 00644
+    notifies :restart, 'service[apache2]'
+    action :create_if_missing
+    variables(
+      :ip => site['ip'],
+      :name => site['name']
+    )
+  end
+  
+  apache_site site['name'] do
+    enable
+  end
+end
