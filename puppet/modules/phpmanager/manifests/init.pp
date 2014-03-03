f4a4a4733c1ba24a7c9567181510e991134d40af
Steven Rombauts <steven@timble.net>
2014-03-03 15:12:27 +0100

    re #87 - Fix provisioning of legacy OpenSSL re #87 - Fix provisioning chain for phpmanager module re #87 - Fix cURL for 5.2.0

--- puppet/modules/phpmanager/manifests/init.pp
+++ puppet/modules/phpmanager/manifests/init.pp
@@ -13,7 +13,7 @@ class phpmanager::install {
     source => 'puppet:///modules/phpmanager/scripts',
     recurse => true,
     owner    => vagrant,
-    group    => vagrant,
+    group    => vagrant
   }
 
   file { $phpmanager::source_path:
@@ -21,6 +21,7 @@ class phpmanager::install {
     owner  => vagrant,
     group  => vagrant,
     mode   => 755,
+    require => Package['git-core']
   }
 
   file { $phpmanager::php_source_path:
@@ -67,20 +68,21 @@ class phpmanager::buildtools {
   package { ['autoconf2.13', 're2c', 'apache2-prefork-dev', 'bison']: ensure  => 'installed' }
   package { ['libcurl4-openssl-dev', 'libmysqlclient-dev', 'libmcrypt-dev', 'libbz2-dev', 'libjpeg-dev', 'libpng12-dev', 'libfreetype6-dev', 'libicu-dev', 'libxml2-dev', 'libxslt-dev', 'libssl-dev']: ensure => 'installed' }
 
-  puppi::netinstall { 'bison':
+  puppi::netinstall { 'bison-2.2':
     url => 'http://ftp.gnu.org/gnu/bison/bison-2.2.tar.gz',
     extracted_dir => 'bison-2.2',
     destination_dir => $phpmanager::source_path,
-    postextract_command => "${phpmanager::source_path}/bison-2.2/configure --prefix=${phpmanager::installation_path}/bison-2.2 && make && sudo make install"
+    postextract_command => "${phpmanager::source_path}/bison-2.2/configure --prefix=${phpmanager::installation_path}/bison-2.2 && make && sudo make install",
+    require => Package['build-essential']
   }
 
-  puppi::netinstall { 'flex':
+  puppi::netinstall { 'flex-2.5.4a':
     path => ["${phpmanager::installation_path}/bison-2.2/bin:/bin:/sbin:/usr/bin:/usr/sbin"],
     url => 'http://fossies.org/unix/misc/old/flex-2.5.4a.tar.gz',
     extracted_dir => 'flex-2.5.4',
     destination_dir => $phpmanager::source_path,
     postextract_command => "${phpmanager::source_path}/flex-2.5.4/configure --prefix=${phpmanager::installation_path}/flex-2.5.4 && make && sudo make install",
-    require => Puppi::Netinstall['bison'],
+    require => Puppi::Netinstall['bison-2.2'],
   }
 
   puppi::netinstall { 'mysql-5.1.73':
@@ -90,11 +92,18 @@ class phpmanager::buildtools {
     postextract_command => "ln -s ${phpmanager::installation_path}/mysql-5.1.73-linux-x86_64-glibc23/lib ${phpmanager::installation_path}/mysql-5.1.73-linux-x86_64-glibc23/lib/x86_64-linux-gnu"
   }
 
-  puppi::netinstall { 'openssl':
-    path => ["${phpmanager::installation_path}/bison-2.2/bin:/bin:/sbin:/usr/bin:/usr/sbin"],
+  puppi::netinstall { 'openssl-0.9.7g':
     url => 'http://www.openssl.org/source/openssl-0.9.7g.tar.gz',
     extracted_dir => 'openssl-0.9.7g',
     destination_dir => $phpmanager::source_path,
-    postextract_command => "${phpmanager::source_path}/openssl-0.9.7g/config --prefix=${phpmanager::installation_path}/openssl-0.9.7g -fPIC no-gost && make && sudo make install && ln -s /opt/openssl-0.9.7g/lib x86_64-linux-gnu"
+    postextract_command => "${phpmanager::source_path}/openssl-0.9.7g/config --prefix=${phpmanager::installation_path}/openssl-0.9.7g -fPIC no-gost && make && sudo make install && ln -s /opt/openssl-0.9.7g/lib /opt/openssl-0.9.7g/lib/x86_64-linux-gnu",
+    require => Package['build-essential']
+  }
+
+  puppi::netinstall { 'curl-7.15.3':
+    url => 'http://ftp.sunet.se/pub/www/utilities/curl/curl-7.15.3.tar.gz',
+    extracted_dir => 'curl-7.15.3',
+    destination_dir => $phpmanager::source_path,
+    require => File["$phpmanager::source_path"]
   }
 }
