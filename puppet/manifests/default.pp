fa121d23fa11f1287177694f83bc82cf7e4c6dc5
Ercan Ozkaya <ercan@timble.net>
2014-04-25 14:35:46 +0300

    Set a JOOMLATOOLS_BOX environment variable in Apache

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -200,6 +200,12 @@ apache::vhost { 'default':
   template     => 'apache/virtualhost/joomlatools.vhost.conf.erb',
 }
 
+exec { 'set-env-for-debugging':
+  command => "echo \"\nSetEnv JOOMLATOOLS_BOX 1\" >> /etc/apache2/apache2.conf",
+  unless  => "grep JOOMLATOOLS_BOX /etc/apache2/apache2.conf",
+  notify  => Service['apache']
+}
+
 class { 'scripts': }
 
 class { 'phpmanager': }
