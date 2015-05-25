6552fe481e0314d12e9f692ce315b116ef0276f1
Steven Rombauts <steven@timble.net>
2015-05-25 18:39:06 +0100

    re #89 - Split the original apc script into multiple subcommands

---
+++ puppet/modules/box/files/scripts/Command/apc/Enable.php
@@ -0,0 +1,28 @@
+<?php
+namespace Command\Apc;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Enable extends Apc
+{
+    protected function configure()
+    {
+        $this->setName('apc:enable')
+             ->setDescription('Enable APC');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $files = $this->_getConfigFiles($this->_ini_files);
+
+        foreach($files as $file) {
+            `sudo sed -i 's#^; extension=#extension=#' $file`;
+        }
+
+        `sudo /etc/init.d/apache2 restart`;
+
+        $output->writeln('Done');
+    }
+}
