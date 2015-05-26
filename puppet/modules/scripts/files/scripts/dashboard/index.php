8a6db8535f56a1377a8c519c580fe15f74ce1154
Steven Rombauts <steven@timble.net>
2015-05-26 16:56:52 +0100

    re #150 - Add Terminal to dashboard, add getting started instructions to MOTD

--- puppet/modules/scripts/files/scripts/dashboard/index.php
+++ puppet/modules/scripts/files/scripts/dashboard/index.php
@@ -92,6 +92,7 @@ foreach ($dir as $fileinfo)
             <?php if (function_exists('apc_cache_info') && @apc_cache_info('opcode')): ?>
                 <li><a href="/apc">APC dashboard</a></li>
             <?php endif; ?>
+            <li><a href="http://joomla.box:3000/">Terminal</a></li>
           </ul>
         </div>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
