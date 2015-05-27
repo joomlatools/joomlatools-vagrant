f10d4622c91bc5595d70111ff148c8b55fdcf24c
Steven Rombauts <steven@timble.net>
2015-05-27 11:47:32 +0100

    re #144 - Add MySQL log and syslog

--- puppet/modules/pimpmylog/manifests/init.pp
+++ puppet/modules/pimpmylog/manifests/init.pp
@@ -17,13 +17,27 @@ class pimpmylog {
         require => File['/usr/share/pimpmylog']
     }
 
-    exec { 'make-apache-logs-world-readable':
+    exec { 'make-apache-logrotate-world-readable':
         command => 'sed -i \'s/apache2\/\*.log/apache2\/*log/g\' /etc/logrotate.d/apache2 && sed -i \'s/create [0-9]\+ root adm/create 644 root adm/g\' /etc/logrotate.d/apache2',
         notify  => Service['apache']
     }
 
-    exec { 'make-log-directories-world-readable':
-        command => 'find /var/log -type d -exec chmod 755 {} \; && find /var/log/apache2 -type f -exec chmod 644 {} \; ',
+    exec { 'make-apache-logs-world-readable':
+        command => 'find /var/log/apache2 -type f -exec chmod 644 {} \;',
         notify  => Service['apache']
+    }
+
+    exec { 'make-mysql-logs-world-readable':
+        command => 'find /var/log/mysql -type f -exec chmod 644 {} \;',
+        notify  => Service['mysql']
+    }
+
+    exec { 'make-log-directories-world-readable':
+        command => 'find /var/log -type d -exec chmod 755 {} \;',
+        notify  => [Service['apache'], Service['mysql']]
+    }
+
+    exec { 'make-system-logs-world-readable':
+        command => 'chmod 644 /var/log/syslog'
     }
 }
