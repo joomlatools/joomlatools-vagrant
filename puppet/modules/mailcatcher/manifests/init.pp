ba0232a8cdbc484513e480ef2eb1f69ce9c8f7e9
Steven Rombauts <steven@timble.net>
2015-05-28 11:12:45 +0100

    Simplify Mailcatcher class

--- puppet/modules/mailcatcher/manifests/init.pp
+++ puppet/modules/mailcatcher/manifests/init.pp
@@ -1,31 +1,25 @@
-class mailcatcher{
-  class{"mailcatcher::packages":}
-  -> class{"mailcatcher::configs":}
-  -> class{"mailcatcher::service":}
-}
+class mailcatcher {
 
-class mailcatcher::packages{
-  package{["sqlite3", "libsqlite3-dev"]:}
-  -> exec {'install-mailcatcher-gem':
+  package { ["sqlite3", "libsqlite3-dev"]: }
+
+  exec {'install-mailcatcher-gem':
         user    => vagrant,
         command => 'bash -c "source ~/.rvm/scripts/rvm; gem install mailcatcher"',
         environment => ['HOME=/home/vagrant'],
         path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/vagrant/.rvm/bin/',
+        require => [Package['sqlite3'], Package['libsqlite3-dev']]
   }
-}
 
-class mailcatcher::configs{
-  file{"/etc/init/mailcatcher.conf":
-    content => template("mailcatcher/upstart.conf.erb")
+  file { '/etc/init/mailcatcher.conf':
+    content => template("mailcatcher/upstart.conf.erb"),
+    require => Exec['install-mailcatcher-gem']
   }
-}
 
-class  mailcatcher::service{
-  # mailcatcher  --http-ip 0.0.0.0
   service{"mailcatcher":
     ensure   => running,
     provider => upstart,
     hasstatus => true,
-    subscribe => Class['mailcatcher::configs']
+    subscribe => File['/etc/init/mailcatcher.conf'],
+    require => File['/etc/init/mailcatcher.conf']
   }
 }
