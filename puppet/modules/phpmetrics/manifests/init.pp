0ce4013876d3d48f2b120293d684855a4270980b
Steven Rombauts <steven@timble.net>
2015-05-29 18:37:04 +0100

    re #102 - Install phpmetrics outside of the global composer path
    If you change PHP version to 5.3.x, the joomla command will stop working
    because the phpmetrics deps are not compatible (and they get loaded in the autoloader)

--- puppet/modules/phpmetrics/manifests/init.pp
+++ puppet/modules/phpmetrics/manifests/init.pp
@@ -1,11 +1,24 @@
 class phpmetrics {
 
+    file { '/usr/share/phpmetrics':
+        ensure => directory,
+        owner  => vagrant
+    }
+
     exec {'install-phpmetrics':
-        command => 'composer global require halleck45/phpmetrics --no-interaction',
-        cwd     => '/home/vagrant/',
-        unless  => '[ -d /home/vagrant/.composer/vendor/halleck45 ]',
+        command => 'composer require halleck45/phpmetrics --no-interaction',
+        cwd     => '/usr/share/phpmetrics',
+        unless  => '[ -d /usr/share/phpmetrics/vendor/halleck45 ]',
         user    => vagrant,
-        environment => 'COMPOSER_HOME=/home/vagrant/.composer'
+        environment => 'COMPOSER_HOME=/home/vagrant/.composer',
+        require => File['/usr/share/phpmetrics']
+    }
+
+    exec { 'add-phpmetrics-to-path':
+        command => 'echo "export PATH=\$PATH:/usr/share/phpmetrics/vendor/bin" >> /home/vagrant/.profile',
+        unless  => 'grep ":/usr/share/phpmetrics/vendor/bin" /home/vagrant/.profile',
+        user    => vagrant,
+        require => Exec['install-phpmetrics']
     }
 
 }
