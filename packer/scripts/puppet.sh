130e53a20a126a3d1da91feadd321ce9e329f28f
Steven Rombauts <steven@timble.net>
2015-05-30 15:00:20 +0100

    Use puppetlabs-release-trusty in Packer, add Atlas artifact

--- packer/scripts/puppet.sh
+++ packer/scripts/puppet.sh
@@ -1,8 +1,8 @@
 sudo apt-get install -y python-software-properties
 
 cd /tmp
-wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb
-sudo dpkg -i puppetlabs-release-precise.deb
+wget http://apt.puppetlabs.com/puppetlabs-release-trusty.deb
+sudo dpkg -i puppetlabs-release-trusty.deb
 
 apt-get update
 apt-get install -y puppet-common
