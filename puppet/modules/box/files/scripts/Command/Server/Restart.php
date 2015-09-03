540721b6bfe8c8590ef1eb3bdef24f40cc1aa1ac
Steven Rombauts <steven@timble.net>
2015-09-03 13:49:04 +0100

    Add box:restart command

--- puppet/modules/box/files/scripts/Command/Server/Restart.php
+++ puppet/modules/box/files/scripts/Command/Server/Restart.php
@@ -6,19 +6,19 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class Stop extends Command
+class Restart extends Command
 {
     protected function configure()
     {
-        $this->setName('server:stop')
-             ->setDescription('Stop Apache and MySQL');
+        $this->setName('server:restart')
+             ->setDescription('Restart Apache and MySQL');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        `sudo service apache2 stop 2>&1 1> /dev/null`;
-        `sudo service mysql stop 2>&1 1> /dev/null`;
+        `sudo service apache2 restart 2>&1 1> /dev/null`;
+        `sudo service mysql restart 2>&1 1> /dev/null`;
 
-        $output->writeln("Server has been stopped");
+        $output->writeln("Server has been restarted");
     }
 }
