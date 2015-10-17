22712a7abb491e1f719fbb21c63f1b47117e7d1d
Steven Rombauts <steven@timble.net>
2015-10-17 20:55:09 +0100

    Create /terminal and /mailcatcher aliases

--- puppet/modules/scripts/files/scripts/dashboard/index.php
+++ puppet/modules/scripts/files/scripts/dashboard/index.php
@@ -110,7 +110,7 @@ foreach ($dir as $fileinfo)
           <ul class="nav nav-sidebar">
             <li role="presentation" class="dropdown-header">Tools</li>
             <li><a href="http://phpmyadmin.joomla.box/">phpMyAdmin</a></li>
-            <li><a href="http://joomla.box:1080/">MailCatcher</a></li>
+            <li><a href="/mailcatcher">MailCatcher</a></li>
             <?php if (function_exists('apc_cache_info') && @apc_cache_info('opcode')): ?>
                 <li><a href="/apc">APC dashboard</a></li>
             <?php endif; ?>
@@ -119,7 +119,7 @@ foreach ($dir as $fileinfo)
             <li role="presentation" class="dropdown-header">System</li>
               <li><a href="/phpinfo">phpinfo</a></li>
               <li><a href="/pimpmylog">Log Files</a></li>
-              <li><a href="http://joomla.box:3000/">Terminal</a></li>
+              <li><a href="/terminal">Terminal</a></li>
           </ul>
         </div>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
