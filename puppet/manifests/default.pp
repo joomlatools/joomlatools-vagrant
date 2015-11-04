1d1ddcae2bc29608b4109b00aead9915e1cdbb91
Steven Rombauts <steven@timble.net>
2015-11-04 09:56:15 +0000

    Set a default timeout of 900 seconds on Exec

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,5 +1,5 @@
 group { 'puppet': ensure => present }
-Exec { path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/', '/usr/local/bin/' ] }
+Exec { path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/', '/usr/local/bin/' ], timeout => 900 }
 File { owner => 0, group => 0, mode => 0644 }
 
 user { 'vagrant': }
