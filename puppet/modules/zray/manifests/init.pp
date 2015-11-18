bd8e604e36e6a5498c0008aeca633eb49ca5b0b9
Steven Rombauts <steven@timble.net>
2015-11-18 17:46:01 +0000

    re #230 - Do not install composer plugin separately

--- puppet/modules/zray/manifests/init.pp
+++ puppet/modules/zray/manifests/init.pp
@@ -53,6 +53,7 @@ class zray {
       install_directory => '/opt/zray-php-101832-php5.6.11-linux-debian7-amd64/zray/runtime/var/plugins',
       require           => Zray::Php::Ini['zray-php5.6']
     }
+    }
   }
 
   $joomla_default = {
@@ -61,23 +62,5 @@ class zray {
   }
 
   create_resources('zray::plugin', $joomla, $joomla_default)
-
-  $composer = {
-    'composer-zray-php5.5' => {
-      install_directory => '/opt/zray-php-101832-php5.5.27-linux-debian7-amd64/zray/runtime/var/plugins',
-      require           => Zray::Php::Ini['zray-php5.5']
-    },
-    'composer-zray-php5.6' => {
-      install_directory => '/opt/zray-php-101832-php5.6.11-linux-debian7-amd64/zray/runtime/var/plugins',
-      require           => Zray::Php::Ini['zray-php5.6']
-    }
-  }
-
-  $composer_default = {
-    archive_root_dir  => 'Composer-master',
-    archive           => 'https://github.com/zend-server-plugins/Composer/archive/master.zip'
-  }
-
-  create_resources('zray::plugin', $composer, $composer_default)
 
 }
