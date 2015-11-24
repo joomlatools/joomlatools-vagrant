ea97a7160e6bc9972ed5fa69b09846a20ea7b49e
Steven Rombauts <steven@timble.net>
2015-11-24 15:42:05 +0000

    Ensure hostname is always set correctly

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -2,6 +2,10 @@ group { 'puppet': ensure => present }
 Exec { path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/', '/usr/local/bin/' ], timeout => 900 }
 File { owner => 0, group => 0, mode => 0644 }
 
+system::hostname { 'joomlatools':
+  ip => '127.0.1.1'
+}
+
 user { 'vagrant': }
 
 class {'apt':
