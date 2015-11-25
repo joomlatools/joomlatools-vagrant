992b08cda238858d218dee1ea58cf922e9eefac9
Steven Rombauts <steven@timble.net>
2015-11-25 09:29:33 +0000

    Improve cleanup.sh script

--- packer/scripts/cleanup.sh
+++ packer/scripts/cleanup.sh
@@ -1,6 +1,8 @@
 echo "cleaning up apt"
 apt-get -y autoremove
 apt-get -y clean
+apt-get -y autoclean all
+rm -rf /var/lib/apt/lists/*
 
 echo "cleaning up guest additions"
 rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
@@ -14,5 +16,8 @@ mkdir /etc/udev/rules.d/70-persistent-net.rules
 rm -rf /dev/.udev/
 rm /lib/udev/rules.d/75-persistent-net-generator.rules
 
-echo "removing headers"
-rm -rf /usr/src/linux-headers*
+echo "removing unneeded source files"
+rm -rf /usr/src/*
+
+echo "remove documentation"
+rm -rf /usr/share/doc/* /usr/share/lintian/* /usr/share/linda/* /var/cache/man/*
