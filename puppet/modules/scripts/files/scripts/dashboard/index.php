5e4454db246c3378e2ad58f314cef953b0f1171b
Tom Janssens <tom@timble.net>
2016-07-26 16:28:40 +0200

    #13 - Removed support for old IE versions

--- puppet/modules/scripts/files/scripts/dashboard/index.php
+++ puppet/modules/scripts/files/scripts/dashboard/index.php
@@ -84,15 +84,6 @@ foreach ($dir as $fileinfo)
 
     <!-- Custom styles for the dashboard -->
     <link href="assets/css/dashboard.css" rel="stylesheet">
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="assets/javascripts/ie10-viewport-bug-workaround.js"></script>
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
 
   <body>
