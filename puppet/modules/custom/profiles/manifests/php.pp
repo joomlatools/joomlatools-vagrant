36a67d2f79c96c179c2006910c31b7e86258fa0c
Steven Rombauts <steven@timble.net>
2017-08-31 12:33:41 +0100

    #28: Create PHP-FPM Apache config

--- puppet/modules/custom/profiles/manifests/php.pp
+++ puppet/modules/custom/profiles/manifests/php.pp
@@ -3,4 +3,14 @@ class profiles::php {
   include ::php
   include ::php::dev
 
+  file { '/run/php/php-fpm.sock':
+    ensure => link,
+    target => '/run/php/php7.1-fpm.sock',
+    notify => Service['apache']
+  }
+
+  apache::dotconf { 'php-fpm':
+    source  => 'puppet:///modules/profiles/php/apache.conf'
+  }
+
 }
