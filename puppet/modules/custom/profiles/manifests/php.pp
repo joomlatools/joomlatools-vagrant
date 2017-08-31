3fdefd8b8b409a2f44782748ed11de21e46bac5f
Steven Rombauts <steven@timble.net>
2017-08-31 22:19:27 +0100

    #28: Compile php-fpm and use updates-alternatives in phpmanager

--- puppet/modules/custom/profiles/manifests/php.pp
+++ puppet/modules/custom/profiles/manifests/php.pp
@@ -3,6 +3,8 @@ class profiles::php {
   include ::php
   include ::php::dev
 
+  $version = hiera('php::globals::php_version', '7.1')
+
   file { '/run/php/php-fpm.sock':
     ensure => link,
     target => '/run/php/php7.1-fpm.sock',
@@ -11,6 +13,24 @@ class profiles::php {
 
   apache::dotconf { 'php-fpm':
     source  => 'puppet:///modules/profiles/php/apache.conf'
+  }
+
+  exec { 'pecl-alternative':
+    command => "mv /usr/bin/pecl /usr/bin/pecl${version} && /usr/bin/update-alternatives --install /usr/bin/pecl pecl /usr/bin/pecl${version} 70",
+    unless  => "/usr/bin/test -h /usr/bin/pecl",
+    require => Anchor['php::end']
+  }
+
+  exec { 'pear-alternative':
+    command => "mv /usr/bin/pear /usr/bin/pear${version} && /usr/bin/update-alternatives --install /usr/bin/pear pear /usr/bin/pear${version} 70",
+    unless  => "/usr/bin/test -h /usr/bin/pear",
+    require => Anchor['php::end']
+  }
+
+  exec { 'php-fpm-alternative':
+    command => "/usr/bin/update-alternatives --install /usr/sbin/php-fpm php-fpm /usr/sbin/php-fpm${version} 70",
+    unless  => "/usr/bin/test -h /usr/sbin/php-fpm",
+    require => Anchor['php::end']
   }
 
 }
