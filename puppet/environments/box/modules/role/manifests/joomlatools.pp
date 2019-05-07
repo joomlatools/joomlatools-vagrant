5f57f1120cff3ed9768646a78508784bb2fb53fb
yiendos <yiendos@sbmatrix.com>
2019-05-07 17:04:32 +0100

    #139: quickly lay the basis for installing cockpit, still need https cert

--- puppet/environments/box/modules/role/manifests/joomlatools.pp
+++ puppet/environments/box/modules/role/manifests/joomlatools.pp
@@ -1,4 +1,19 @@
 class role::joomlatools inherits role {
+  
+  exec { 'install cockpit':
+    path => '/bin:/usr/bin/:/sbin:/usr/sbin',
+    command => 'apt-get install cockpit'
+  }
+
+  exec { 'create start up':
+    path => '/bin:/usr/bin/:/sbin:/usr/sbin',
+    command => 'systemctl start cockpit.socket'
+  }
+
+  exec { 'enable start up':
+    path => '/bin:/usr/bin/:/sbin:/usr/sbin',
+    command => 'systemctl enable cockpit.socket'
+  }
 
   include ::profiles::apache
   include ::profiles::nginx
