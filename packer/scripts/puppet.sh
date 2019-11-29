0f574c5924db108456bb270084cda4f4df4ed82d
Steven Rombauts <steven@kotuha.be>
2019-11-29 20:27:55 +0100

    #158: Update packer puppet-masterless builder

--- packer/scripts/puppet.sh
+++ packer/scripts/puppet.sh
@@ -5,8 +5,8 @@ set -e
 apt-get install -y software-properties-common python-software-properties
 
 cd /tmp
-wget http://apt.puppetlabs.com/puppetlabs-release-trusty.deb
-dpkg -i puppetlabs-release-trusty.deb
+wget https://apt.puppetlabs.com/puppet5-release-bionic.deb
+dpkg -i puppet5-release-bionic.deb
 
 apt-get update
-apt-get install -y puppet-common
+apt-get install -y puppet
